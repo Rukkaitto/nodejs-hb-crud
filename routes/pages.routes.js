d5aba8d75250fe379d384ce599d2101d744dfe5c
Lucas Goudin <goudinlucas@gmail.com>
2021-04-16 12:02:30 +0200

    Formatting

--- routes/pages.routes.js
+++ routes/pages.routes.js
@@ -2,8 +2,11 @@ module.exports = (express, controller) => {
   const router = express.Router();
 
   router.get("/", controller.getUsers);
+
   router.get("/create", controller.createUser);
+  
   router.get("/:id", controller.getUser);
+
   router.get("/update/:id", controller.updateUser);
 
   return router;
